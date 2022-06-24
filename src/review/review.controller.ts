3e460f56254189820aadcbfbde0c76eaffb57315
Alexander Kuznetsov <gl633bl@gmail.com>
2022-06-24 21:03:52 +0300

    Added: controller for telegram

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -15,17 +15,28 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { INVALID_ID_ERROR, REVIEW_NOT_FOUND } from './review.constans';
 import { JwrAuthGuard } from '../auth/guards/jwt.guard';
-import { UserEmail } from '../decorators/user-email.decorator';
 import { IdValidationPipe } from '../pipes/id-validation.pipe';
+import { TelegramService } from '../telegram/telegram.service';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(
+    private readonly reviewService: ReviewService,
+    private readonly telegramService: TelegramService,
+  ) {}
 
   @UsePipes(new ValidationPipe())
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
     return await this.reviewService.create(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post('notify')
+  async notify(@Body() dto: CreateReviewDto) {
+    const message = `Имя: ${dto.name}\nЗаголовок: ${dto.title}\nОписание: ${dto.description}\nРейтинг: ${dto.rating}\nID продукта: ${dto.productId}\n`;
+
+    return await this.telegramService.sendMessage(message);
   }
 
   @UseGuards(JwrAuthGuard)
